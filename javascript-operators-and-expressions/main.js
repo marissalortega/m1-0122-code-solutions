66008dd7f0cadf50635adb716f1195273c2e1615
Marissa Ortega <marissaljames@gmail.com>
2022-02-14 21:30:34 -0800

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,43 @@
+var width = 5;
+var height = 7;
+
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 20;
+var payment = 25;
+
+var change = payment - bill;
+console.log('value of change', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 88;
+var midterms = 95;
+var final = 92;
+
+var grade = (quizzes + midterms + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Marissa ';
+var lastName = 'Ortega';
+
+var fullName = firstName + lastName;
+console.log('value of full name:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 24;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
